a0773f3b103e95a48fb5a07d9feb30f6106d46ce
Mehul Mohan <mehulmpt@gmail.com>
2019-02-11 16:56:10 +0530

    source code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,7 +6,7 @@ import Register from '../Register'
 import Dashboard from '../Dashboard'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { CssBaseline, CircularProgress } from '@material-ui/core'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import firebase from '../firebase'
 
 const theme = createMuiTheme()
